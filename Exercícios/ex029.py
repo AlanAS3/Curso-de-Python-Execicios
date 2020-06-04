90c2f7223aebd56f623078140b486f0bdd089812
Alan Araújo <Alanaraujo003@gmail.com>
2020-06-04 11:55:36 -0300

    Update ex029.py

--- Exercícios/ex029.py
+++ Exercícios/ex029.py
@@ -3,5 +3,6 @@ Vel = float(input('Qual a velocidade do seu carro? '))
 if Vel > 80.0:
     multa = (Vel - 80.0) * 7
     print(f'Você está acima da velocidade, a sua multa é de {multa}')
+    print('A velocidade permitida é de 80Km/h')
 else:
     print('Você está dentro da velocidade permitida, continue assim')
