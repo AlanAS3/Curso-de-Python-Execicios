eb02cb529bf8567e2a3a92157f0b3662b30bbd03
Alan Araújo <Alanaraujo003@gmail.com>
2020-06-04 12:30:09 -0300

    Update ex032.py

--- Exercícios/ex032.py
+++ Exercícios/ex032.py
@@ -1,6 +1,6 @@
 print('=== Ano Bissexto ===')
 ano = int(input('Digite um ano: '))
-if ano % 4 == 0:
+if ano % 4 == 0 and ano % 400 == 0:
     print('Esse é um ano BISSEXTO')
 else:
     print('Esse NÃO é um ano Bissexto')
